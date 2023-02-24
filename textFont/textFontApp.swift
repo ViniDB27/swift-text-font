4461ca4a9a503a784fde05f1e5ccfb36b794d3a4
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 14:20:10 -0300

    Initial Commit

---
+++ textFont/textFontApp.swift
@@ -0,0 +1,17 @@
+//
+//  textFontApp.swift
+//  textFont
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct textFontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
